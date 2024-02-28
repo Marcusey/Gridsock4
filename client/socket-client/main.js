98e5090df90185b2455b8754237b74d85d0bbddf
RebeckaGothlin <rebecka.gothlin@hotmail.com>
2024-02-28 14:08:41 +0100

    :tada: add foundation basics

---
+++ client/socket-client/main.js
@@ -0,0 +1,25 @@
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+let sendMessage = document.querySelector('#sendMessage');
+let sendBtn = document.querySelector('#sendBtn');
+let chatList = document.querySelector('#chatList');
+
+function updateChat(chat) {
+  let li = document.createElement('li');
+  li.innerText = chat;
+  chatList.appendChild(li);
+}
+
+sendBtn.addEventListener('click', () => {
+  console.log('send chat', sendMessage.value);
+
+  socket.emit('chat', sendMessage.value);
+})
+
+socket.on('chat', (arg) => {
+  console.log('socket', arg);
+
+  updateChat(arg);
+})
---
+++ server/app.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
+app.get('/test', (req, res) => {
+
+    res.send('<h1>Socket</h1>');
+});
+
+io.on('connection', (socket) => {
+    // console.log('connection', socket);
+
+    socket.emit('chat', 'Hello World');
+
+    socket.on('chat', (arg) => {
+        console.log('incoming chat', arg);
+
+        io.emit('chat', arg);
+    })
+});
+
+server.listen(3000);
